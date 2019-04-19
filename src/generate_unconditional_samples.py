8b8db0100a7e32ff25d69b4949c37ea8e3621885
Adam Spannbauer <SpannbauerAdam@gmail.com>
2019-04-19 18:18:22 -0400

    reformat code with black to make pycharm happy

--- src/generate_unconditional_samples.py
+++ src/generate_unconditional_samples.py
@@ -8,8 +8,9 @@ import tensorflow as tf
 
 import model, sample, encoder
 
+
 def sample_model(
-    model_name='117M',
+    model_name="117M",
     seed=None,
     nsamples=0,
     batch_size=1,
@@ -38,27 +39,31 @@ def sample_model(
     """
     enc = encoder.get_encoder(model_name)
     hparams = model.default_hparams()
-    with open(os.path.join('models', model_name, 'hparams.json')) as f:
+    with open(os.path.join("models", model_name, "hparams.json")) as f:
         hparams.override_from_dict(json.load(f))
 
     if length is None:
         length = hparams.n_ctx
     elif length > hparams.n_ctx:
-        raise ValueError("Can't get samples longer than window size: %s" % hparams.n_ctx)
+        raise ValueError(
+            "Can't get samples longer than window size: %s" % hparams.n_ctx
+        )
 
     with tf.Session(graph=tf.Graph()) as sess:
         np.random.seed(seed)
         tf.set_random_seed(seed)
 
         output = sample.sample_sequence(
-            hparams=hparams, length=length,
-            start_token=enc.encoder['<|endoftext|>'],
+            hparams=hparams,
+            length=length,
+            start_token=enc.encoder["<|endoftext|>"],
             batch_size=batch_size,
-            temperature=temperature, top_k=top_k
+            temperature=temperature,
+            top_k=top_k,
         )[:, 1:]
 
         saver = tf.train.Saver()
-        ckpt = tf.train.latest_checkpoint(os.path.join('models', model_name))
+        ckpt = tf.train.latest_checkpoint(os.path.join("models", model_name))
         saver.restore(sess, ckpt)
 
         generated = 0
@@ -70,6 +75,6 @@ def sample_model(
                 print("=" * 40 + " SAMPLE " + str(generated) + " " + "=" * 40)
                 print(text)
 
-if __name__ == '__main__':
-    fire.Fire(sample_model)
 
+if __name__ == "__main__":
+    fire.Fire(sample_model)
